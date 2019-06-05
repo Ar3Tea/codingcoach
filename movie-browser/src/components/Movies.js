8e852f2925037488189611c11334df4053c162d6
AreTea <tbusinessr@gmail.com>
2019-06-04 21:05:53 -0400

    starting more basic components

--- movie-browser/src/components/Movies.js
+++ movie-browser/src/components/Movies.js
@@ -18,12 +18,12 @@ class Featured extends Component {
     render() {
       return (
         <div className="bg-blue-400">
-          <h1 className=" text-center font-mono text-3xl italic">Featured Movies</h1>
+          <h1 className=" text-center font-mono text-3xl italic">All Movies</h1>
           <div className="flex flex-row flex-wrap items-center">
-            {this.state.movies.slice(0, 6).map(movie => 
-            <div className="w-1/6 mb-4 text-center border-solid border-4 border-gray-400 pt-6 pb-5">
+            {this.state.movies.map(movie => 
+            <div className="w-1/5 text-center border-solid border-4 border-black m-8 shadow-lg">
             <div className="font-semibold" key={movie.id}>{movie.title}</div>
-            <div key={movie.id}>{movie.rt_score}</div>
+            <div>{movie.rt_score}</div>
             <div>{movie.description.slice(0, 50)}...Read More</div>
           </div>
           )}
